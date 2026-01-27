2c63853d59bd45b14f1dff5b1f8eb982847fe7d8
manoj9329 <manojsmanu9329@gmail.com>
2026-01-27 20:22:10 +0530

    updated program

---
+++ reverse_String_array.java
@@ -0,0 +1,28 @@
+public class reverse_String_array {
+    public void reverse_String(char[] s){
+        int i = 0;
+        int j = s.length-1;
+
+        while (i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+
+    }
+    public static void main(String[] args) {
+        reverse_String_array obj = new reverse_String_array();
+
+        char[] arr = {'h','e','l','l','o'};
+        obj.reverse_String(arr);
+
+        // printing reversed array
+        for (char c : arr) {
+            System.out.print(c + " ");
+    }
+
+    
+}}
